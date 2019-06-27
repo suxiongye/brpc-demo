14b09ea8f3b3c60738c0a9faff4942c1a50e1fe1
suxiongye <suxiongye@baidu.com>
2019-06-27 20:55:02 +0800

    save

---
+++ src/main/java/com/su/brpc/jprotobuf/BenchmarkTest.java
@@ -0,0 +1,132 @@
+package com.su.brpc.jprotobuf;
+
+import com.baidu.brpc.client.BrpcProxy;
+import com.baidu.brpc.client.RpcCallback;
+import com.baidu.brpc.client.RpcClient;
+import com.baidu.brpc.client.RpcClientOptions;
+import com.baidu.brpc.client.channel.ChannelType;
+import com.baidu.brpc.client.loadbalance.LoadBalanceStrategy;
+import com.baidu.brpc.protocol.Options;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * 基准测试（性能测试）
+ *
+ * @author suxiongye
+ * @date 2019-06-27 19:40
+ */
+@Slf4j
+public class BenchmarkTest {
+    private static volatile boolean stop = false;
+
+    public static void main(String[] args) {
+        String serviceUrl;
+        int threadNum;
+        // 设置默认参数
+        if (args.length != 2) {
+            System.out.println("Usage : BenchmarkTest 127.0.0.1:8002 threadNUm");
+            serviceUrl = "list:127.0.0.1:8002";
+            threadNum = 50;
+        } else {
+            serviceUrl = args[0];
+            threadNum = Integer.parseInt(args[1]);
+        }
+        // 配置常用参数
+        RpcClientOptions rpcClientOptions = new RpcClientOptions();
+        rpcClientOptions.setProtocolType(Options.ProtocolType.PROTOCOL_BAIDU_STD_VALUE);
+        rpcClientOptions.setLoadBalanceType(LoadBalanceStrategy.LOAD_BALANCE_FAIR);
+        rpcClientOptions.setMaxTotalConnections(1000000);
+        rpcClientOptions.setMinIdleConnections(10);
+        rpcClientOptions.setConnectTimeoutMillis(1000);
+        rpcClientOptions.setWriteTimeoutMillis(1000);
+        rpcClientOptions.setReadTimeoutMillis(5000);
+        rpcClientOptions.setTcpNoDelay(false);
+        rpcClientOptions.setChannelType(ChannelType.SINGLE_CONNECTION);
+
+        RpcClient rpcClient = new RpcClient(serviceUrl, rpcClientOptions, null);
+        EchoServiceAsync echoServiceAsync = BrpcProxy.getProxy(rpcClient, EchoServiceAsync.class);
+
+        // 从文件读取数据流
+        byte[] messageBytes = null;
+        InputStream inputStream = Thread.currentThread().getClass().getResourceAsStream("/message_1k.txt");
+        try {
+            int length = inputStream.available();
+            messageBytes = new byte[length];
+            inputStream.read(messageBytes);
+            log.info("message size = " + messageBytes.length);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        // 多线程并发请求
+        SendInfo[] sendInfos = new SendInfo[threadNum];
+        Thread[] threads = new Thread[threadNum];
+        for (int i = 0; i < threadNum; i++) {
+            sendInfos[i] = new SendInfo();
+            threads[i] = new Thread(new ThreadTask)
+        }
+
+        long lastSuccessRequestNum = 0;
+        long lastFailRequestNum = 0;
+        long lastElapsedNs = 0;
+        int second = 0;
+        long skippedQps = 0;
+        while (!stop) {
+            long beginTime = System.nanoTime();
+            try {
+                Thread.sleep(1000);
+                ++second;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            long successNum = 0;
+            long failNum = 0;
+            long elapseNs = 0;
+            long averageElapsedNs = 0;
+
+            for (SendInfo sendInfo : se)
+        }
+    }
+
+    // 统计信息
+    public static class SendInfo {
+        public long successRequestNum = 0;
+        public long failRequestNum = 0;
+        public long elapsedNs = 0;
+    }
+
+    // 回调任务
+    public static class EchoCallback implements RpcCallback
+
+    // 线程任务
+    public static class ThreadTask implements Runnable {
+        EchoServiceAsync echoServiceAsync;
+        private RpcClient rpcClient;
+        private byte[] messageBytes;
+        private SendInfo sendInfo;
+
+        public ThreadTask(RpcClient rpcClient, EchoServiceAsync echoServiceAsync, byte[] messageBytes,
+                          SendInfo sendInfo) {
+            this.rpcClient = rpcClient;
+            this.echoServiceAsync = echoServiceAsync;
+            this.messageBytes = messageBytes;
+            this.sendInfo = sendInfo;
+        }
+
+        @Override public void run() {
+            // 发送请求
+            EchoRequest request = new EchoRequest();
+            request.setMessage(new String(messageBytes));
+            while (!stop) {
+                try {
+                    echoServiceAsync.echo(request, new EchoC)
+                }
+            }
+        }
+    }
+}
+
--- src/main/java/com/su/brpc/jprotobuf/RpcClientTest.java
+++ src/main/java/com/su/brpc/jprotobuf/RpcClientTest.java
@@ -11,10 +11,10 @@ import com.baidu.brpc.protocol.Options;
 import com.su.brpc.interceptor.CustomInterceptor;
 import com.su.brpc.jprotobuf.re.query.ReQueryRequest;
 import com.su.brpc.jprotobuf.re.query.ReQueryResponse;
-import io.netty.channel.Channel;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Future;
 
 /**
  * 客户端测试
@@ -25,10 +25,10 @@ import java.util.List;
 public class RpcClientTest {
     public static void main(String[] args) {
         RpcClientOptions clientOptions = new RpcClientOptions();
-        /**
-         * 协议类型，brpc支持的协议包括
-         */
+
+        // 协议类型
         clientOptions.setProtocolType(Options.ProtocolType.PROTOCOL_BAIDU_STD_VALUE);
+        // 超时设置
         clientOptions.setWriteTimeoutMillis(10000);
         clientOptions.setReadTimeoutMillis(10000);
         clientOptions.setMaxTotalConnections(1000);
@@ -36,7 +36,7 @@ public class RpcClientTest {
         clientOptions.setLoadBalanceType(LoadBalanceStrategy.LOAD_BALANCE_FAIR);
         clientOptions.setCompressType(Options.CompressType.COMPRESS_TYPE_NONE);
 
-        String serviceUrl = "bns://im-server.NOVA.all:rt";
+        String serviceUrl = "list://127.0.0.1:8002";
         if (args.length == 1) {
             serviceUrl = args[0];
         }
@@ -48,14 +48,6 @@ public class RpcClientTest {
         EchoRequest echoRequest = new EchoRequest();
         echoRequest.setMessage("Hello world!");
 
-        ReService reService = BrpcProxy.getProxy(rpcClient, ReService.class);
-        try {
-            ReQueryResponse reQueryResponse = reService.reQuery(new ReQueryRequest());
-        }catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
-
         // 同步传输
         EchoService echoService = BrpcProxy.getProxy(rpcClient, EchoService.class);
         try {
@@ -63,20 +55,44 @@ public class RpcClientTest {
             System.out.printf("Syn call service = EchoService.echo success, request = %s, response = %s\n",
                     echoRequest.getMessage(), response.getMessage());
         } catch (RpcException ex) {
-            System.out.println("Sync call faild, ex = " +  ex.getMessage());
+            System.out.println("Sync call faild, ex = " + ex.getMessage());
         }
         rpcClient.stop();
 
         // 测试异步
         RpcCallback callback = new RpcCallback<EchoResponse>() {
+            // 回调成功
             @Override public void success(EchoResponse echoResponse) {
-
+                if (null != echoResponse) {
+                    System.out.printf("Sync call service = EchoService.echo success, response=%s\n",
+                            echoResponse.getMessage());
+                }
             }
 
+            // 回调失败
             @Override public void fail(Throwable throwable) {
-
+                System.out.printf("Sync call service = EchoService.echo fail, %s\n",
+                        throwable.getMessage());
             }
         };
+
+        // 重新设定rpcClient，否则会抛异常
+        rpcClient =  new RpcClient(serviceUrl, clientOptions, interceptors);
+        EchoServiceAsync echoServiceAsync = BrpcProxy.getProxy(rpcClient, EchoServiceAsync.class);
+        try {
+            Future<EchoResponse> responseFuture = echoServiceAsync.echo(echoRequest, callback);
+            try {
+                // 等待回调
+                if (responseFuture != null) {
+                    responseFuture.get();
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } catch (RpcException ex) {
+            System.out.println("RPC send failed, ex=" + ex.getMessage());
+        }
+
         rpcClient.stop();
     }
 }
